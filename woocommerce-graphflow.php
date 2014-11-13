190d24621ca783b946f87eed888b7bb67af7b0ff
mlnick <mlnick@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-13 16:15:20 +0000

    Add version 1.0.5 tag
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-recommendations-by-graphflow/tags/1.0.5@1025195 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ woocommerce-graphflow.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ 	Plugin Name: WooCommerce Recommendations by Graphflow
+ 	Plugin URI: http://www.woothemes.com/products/woocommerce-recommendations/
+	Description: Recommendations for WooCommerce, powered by the Graphflow recommendation engine
+	Author: Gerhard Potgieter / Graphflow
+	Author URI: http://www.graphflow.com/
+	Version: 1.0.5
+	Requires at least: 3.9
+	Tested up to: 4.0
+
+	@package WooCommerce
+	@author WooThemes
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if ( ! function_exists( 'is_woocommerce_active' ) )
+	require_once( 'woo-includes/woo-functions.php' );
+
+// Check if WooCommerce is active
+if ( ! is_woocommerce_active() )
+	return;
+
+function gf_activation_hook() {
+	update_option( 'woocommerce_graphflow_install_notice', false );
+}
+register_activation_hook( __FILE__, 'gf_activation_hook' );
+
+require_once( 'includes/class-wc-graphflow.php' );
+
+$GLOBALS['wc_graphflow'] = new WC_GraphFlow( __FILE__ );
